5038bc7121a77c1966af4371b236267aa3c9e77c
Serhii <serhii.nikiforenko@gmail.com>
2021-05-01 21:49:12 +0300

    componentDidUpdate, componentWillUnmount

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -17,7 +17,10 @@ export default class RandomPlanet extends Component {
   componentDidMount() {
     this.updatePlanet();
     this.interval = setInterval(this.updatePlanet, 2500);
-    // clearInterval(this.interval);
+  }
+
+  componentDidMount() {
+    clearInterval(this.interval);
   }
 
   componentWillUnmount() {
